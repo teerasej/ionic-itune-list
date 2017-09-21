5073e3c217e49e18b0991249e3683d7d41239fea
Teerasej <teerasej@gmail.com>
2017-09-21 15:45:05 +0700

    Init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Http } from "@angular/http";
+import "rxjs/add/operator/map";
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  musicArray = [];
 
+  constructor(public navCtrl: NavController, public http: Http) {
+    let url = "https://itunes.apple.com/search?term=bodyslam";
+
+    this.http.get(url)
+      .map(res => res.json())
+      .subscribe(data => {
+        console.log(data.results);
+        this.musicArray = data.results;
+      })
   }
 
 }
